7eb4c31f95dac37a6c49dcb6870dd4c3539a9ce9
Kyehwan Lee <kyehwanl@nist.gov>
2019-03-13 00:15:12 -0400

    auto-updated 03/13/19 00:15:12

--- Shell_Script/srx_grpc_sync_vm005.sh
+++ Shell_Script/srx_grpc_sync_vm005.sh
@@ -3,10 +3,10 @@
 echo "srx gprc sync with github..."
 cp -rf /opt/project/gobgp_test/gowork/src/srx_grpc /tmp/
 cd /tmp/srx_grpc
+git add --all ./
+git commit -m "auto-updated $(date +"%D %T")"
 git remote add github_repo https://github.com/kyehwanl/srx_grpc.git
 ~/Emulab/Shell_Script/input-expect-github_repo.sh
 rm -rf /tmp/srx_grpc
 echo "Done"
-
-
 
