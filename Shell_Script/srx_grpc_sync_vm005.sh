a6593ce2b979d5852092e8a377ca38d8e03bac90
Kyehwan Lee <kyehwanl@nist.gov>
2019-04-11 00:15:08 -0400

    auto-updated 04/11/19 00:15:08

--- Shell_Script/srx_grpc_sync_vm005.sh
+++ Shell_Script/srx_grpc_sync_vm005.sh
@@ -3,18 +3,26 @@ echo "srx gprc sync with github..."
 cd /tmp
 git clone https://github.com/kyehwanl/srx_grpc.git srx_grpc
 
-git clone /opt/project/gobgp_test/gowork/src/srx_grpc /tmp/srx_grpc0
+rsync -avPquiz --delete /opt/project/gobgp_test/gowork/src/srx_grpc/ /tmp/_srx_grpc/
+cd /tmp/_srx_grpc
+STR=$(git log --graph --oneline --decorate --branches | head -n 1 | grep HEAD | awk -F ")" '{print $2}')
+git add --all ./
+git commit -m "temp-updated $(date +"%D %T")"
+
+cd /tmp
+git clone /tmp/_srx_grpc /tmp/srx_grpc0
 rsync -avPuiz --delete srx_grpc0/ srx_grpc/ --exclude='.git'
 
-cd srx_grpc
+cd /tmp/srx_grpc
 git add --all ./
-git commit -m "auto-updated $(date +"%D %T")"
+git commit -m "auto-updated $(date +"%D %T") $STR"
 
 cp ~/Emulab/Shell_Script/input-expect.sh /tmp/
 /tmp/input-expect.sh
 
 rm -rf /tmp/srx_grpc0
 rm -rf /tmp/srx_grpc
+rm -rf /tmp/_srx_grpc
 rm -rf /tmp/input-expect.sh
 echo "Done"
 
