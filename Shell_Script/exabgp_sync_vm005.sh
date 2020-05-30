8d7c80b6bfdb6f6fc49964d9518c1a5231399f6f
Kyehwan Lee <kyehwanl@nist.gov>
2020-05-30 00:15:07 -0400

    auto-updated 05/30/20 00:15:07

--- Shell_Script/exabgp_sync_vm005.sh
+++ Shell_Script/exabgp_sync_vm005.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -x
 
-echo "ExaBGP sync with github..."
-cp -rf ~/Emulab/exabgp-3.4.13/ /tmp/
-cd /tmp/exabgp-3.4.13
-git remote add origin https://github.com/kyehwanl/exabgp_priv
-~/Emulab/Shell_Script/input-expect.sh
-rm -rf /tmp/exabgp-3.4.13
+echo "ExaBGPSec sync with github..."
+rsync -avPuiz /opt/project/exabgp_test/exabgp-4.1.2 /tmp/
+cd /tmp/exabgp-4.1.2
+git remote add github_repo https://github.com/kyehwanl/exabgpsec_priv.git 
+~/Emulab/Shell_Script/input-expect-github_repo.sh
+rm -rf /tmp/exabgp-4.1.2
 echo "Done"
 
 
--- Shell_Script/gobgp_sync_vm005.sh
+++ Shell_Script/gobgp_sync_vm005.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -x
 
-echo "GoBGP sync with github..."
-cp -rf /opt/project/gobgp_test/gowork/src/github.com/osrg/gobgp/ /tmp/
+echo "GoBGPSec sync with github..."
+rsync -avPuiz /opt/project/gobgp_test/gobgp_v2_running_test/src/github.com/osrg/gobgp /tmp/
 cd /tmp/gobgp
-git remote add github_repo https://github.com/kyehwanl/gobgp_priv
+git remote add github_repo https://github.com/kyehwanl/gobgpsec_priv.git
 ~/Emulab/Shell_Script/input-expect-github_repo.sh
 rm -rf /tmp/gobgp
 echo "Done"
