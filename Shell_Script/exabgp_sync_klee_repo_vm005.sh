6d777a7ec9ff8a4d8c36e424ba7e38881ac3bb16
Kyehwan Lee <kyehwanl@nist.gov>
2020-06-18 00:15:08 -0400

    auto-updated 06/18/20 00:15:08

--- Shell_Script/exabgp_sync_klee_repo_vm005.sh
+++ Shell_Script/exabgp_sync_klee_repo_vm005.sh
@@ -4,6 +4,11 @@ echo "ExaBGPSec sync with github..."
 rsync -avPuiz /opt/project/exabgp_test/exabgp-4.1.2 /tmp/
 cd /tmp/exabgp-4.1.2
 git remote add github_repo https://github.com/kyehwanlee/exabgpsec_temp.git
+
+STR=$(git log --graph --oneline --decorate --branches | head -n 1 | grep HEAD | awk -F ")" '{print $2}')
+git add --all ./
+git commit -m "auto-updated $(date +"%D %T") $STR"
+
 ~/Emulab/Shell_Script/input-expect-kyehwanlee_repo.sh
 rm -rf /tmp/exabgp-4.1.2
 echo "Done"
