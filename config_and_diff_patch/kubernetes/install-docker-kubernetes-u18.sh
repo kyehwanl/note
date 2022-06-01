181fa3bd9670fe647b9e779100a52b8fbe6d19fc
Kyehwan Lee <kyehwanl@nist.gov>
2022-06-01 01:15:08 -0400

    auto-updated 06/01/22 01:15:08

---
+++ config_and_diff_patch/kubernetes/install-docker-kubernetes-u18.sh
@@ -0,0 +1,51 @@
+#! /bin/sh 
+set -x
+
+#1. Install Docker
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# (Ubuntu 18.04)
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" 
+# (Ubuntu 20.04)
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+sudo apt update -y
+sudo apt install -y docker-ce
+
+
+
+#2. Install Kubernetes
+
+
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt install -y firewalld 
+sudo firewall-cmd --permanent --add-port=6443/tcp
+sudo firewall-cmd --permanent --add-port=10250/tcp
+sudo firewall-cmd --add-masquerade --permanent
+sudo firewall-cmd --reload
+
+
+sudo apt-get install apt-transport-https curl -y
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb https://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt-get install kubeadm -y
+sudo swapoff -a
+
+
+sudo rm /etc/containerd/config.toml
+sudo systemctl restart containerd
+sudo kubeadm init --pod-network-cidr 192.168.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
+kubectl taint nodes --all node-role.kubernetes.io/control-plane:NoSchedule-
+kubectl taint nodes --all node-role.kubernetes.io/master:NoSchedule-
+
+
+
