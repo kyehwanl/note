ce36d2045cddfa56566544e8f6de288177f419a5
Kyehwan Lee <kyehwanl@nist.gov>
2022-08-04 01:15:08 -0400

    auto-updated 08/04/22 01:15:08

---
+++ config_and_diff_patch/kubernetes/install-minikube-ubuntu.sh
@@ -0,0 +1,35 @@
+#! /bin/sh 
+set -x
+
+# 1. Install Docker
+sudo apt update -y
+sudo apt upgrade -y
+
+
+#2. Install Minikube dependencies
+sudo apt install -y curl wget apt-transport-https
+
+
+#3. Download Minikube Binary
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo cp minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod +x /usr/local/bin/minikube
+minikube version
+
+
+#4. Install Kubectl utility
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+
+#5. Install Docker
+sudo apt install docker.io -y
+sudo usermod -aG docker ${USER}
+newgrp docker
+
+#6. Start the minikube
+minikube start --driver=docker
+
+
+
