0a815a6f226f5d60e4688d3277537f69c4364648
Kyehwan Lee <kyehwanl@nist.gov>
2022-12-24 01:15:16 -0500

    auto-updated 12/24/22 01:15:16

---
+++ Shell_Script/install_docker-ce_on_ubuntu.sh
@@ -0,0 +1,20 @@
+#! /bin/bash -x
+
+# system update
+#sudo apt update && sudo apt upgrade -y
+sudo apt update -y
+sudo apt-get -y install curl apt-transport-https ca-certificates software-properties-common
+
+
+# Add the Docker Repositories
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update -y
+
+
+# Install Docker on Ubuntu 18.04
+sudo apt install -y docker-ce
+
+
+
+
