772ccd79af43b6e9d57418709c75756bffa7c77c
Kyehwan Lee <kyehwanl@nist.gov>
2023-02-01 01:15:12 -0500

    auto-updated 02/01/23 01:15:12

--- Shell_Script/install_vagrant_virtualbox_emulab.sh
+++ Shell_Script/install_vagrant_virtualbox_emulab.sh
@@ -4,6 +4,11 @@ sudo apt install -y vagrant virtualbox
 vboxmanage setproperty machinefolder /proj/BGPSec/vagrant-work/VirtualBoxVMs
 # echo "export VAGRANT_HOME=/proj/BGPSec/vagrant-work/.vagrant.d/" >> ~/.bash_profile
 #cd /proj/BGPSec/vagrant-work/vagrant-images/ && mkdir -p test1 && vagrant init bento/ubuntu-18.04
+if [ $VAGRANT_HOME == "" ]; then
+  echo env missing ..
+  echo "export VAGRANT_HOME=/proj/BGPSec/vagrant-work/.vagrant.d/" >> ~/.bash_profile
+  export VAGRANT_HOME=/proj/BGPSec/vagrant-work/.vagrant.d/
+fi
 #vagrant up
 
 
