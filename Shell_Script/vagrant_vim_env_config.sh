99db8498283398b2bccdbfccfece4df247387ded
Kyehwan Lee <kyehwanl@nist.gov>
2023-02-18 01:15:10 -0500

    auto-updated 02/18/23 01:15:10

--- Shell_Script/vagrant_vim_env_config.sh
+++ Shell_Script/vagrant_vim_env_config.sh
@@ -4,6 +4,8 @@ cd $HOME
 
 # download vim files
 wget https://github.com/kyehwanl/config_ready_to_use/raw/master/.vimrc
+wget https://github.com/kyehwanl/config_ready_to_use/raw/master/.gitconfig
+wget https://github.com/kyehwanl/config_ready_to_use/raw/master/.bash_aliases
 wget https://github.com/kyehwanl/config_ready_to_use/raw/master/_.vim_2023_0203.tar.gz 
 tar xvfz _.vim_2023_0203.tar.gz 
 
